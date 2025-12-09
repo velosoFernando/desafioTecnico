e17736364e731659569904fccbb126e3aeea2e7f
Fernando Veloso <fernandovelosomkt@gmail.com>
2025-12-09 00:41:39 -0300

    test: Finalização da checagem automatizada!

--- cypress/e2e/shopperAmazon.cy.js
+++ cypress/e2e/shopperAmazon.cy.js
@@ -1,1 +1,8 @@
-
+describe("Amazon – Jornada do Usuário", () => {
+  it("TestCase: Pesquisar Produto e Adicionar ao Carrinho", () => {
+    cy.start();
+    cy.searchProduct();
+    cy.addProductToCart();
+    cy.validCart();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,35 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Facilita o start (definindo resoulução e site que iremos visitar).
+Cypress.Commands.add("start", () => {
+  cy.viewport(1920, 1080); // Resolução FULL HD.
+  cy.visit("https://www.amazon.com.br/");
+  cy.get("#nav-logo-sprites").should("be.visible");
+});
+
+// Pesquisa pelo produto.
+Cypress.Commands.add("searchProduct", () => {
+  cy.get("#twotabsearchtextbox").click().type("Nintendo Switch");
+  cy.get("#nav-search-submit-button").click();
+  cy.get("img.s-image").eq(1).click({ force: true });
+  cy.contains(
+    /Case Capa Protetora Compatível com Nintendo Switch e OLED/i
+  ).should("be.visible"); // Evita falgas caso haja espaços invisíveis
+});
+
+// Adiciona o produto ao carrinho.
+Cypress.Commands.add("addProductToCart", () => {
+  cy.get("#add-to-cart-button").click();
+  cy.contains("h1", "Adicionado ao carrinho").should("be.visible");
+});
+
+// Valida a presença do produto no carrinho
+Cypress.Commands.add("validCart", () => {
+  cy.contains("a", "Ir para o carrinho").click();
+  cy.contains(
+    "span.a-label.a-checkbox-label",
+    "Este pedido contém um presente"
+  ).should("be.visible");
+});
